73e9973fd237bebb1685752f0c56c187a65891ef
f-o-o-g-s <17360806+f-o-o-g-s@users.noreply.github.com>
2024-12-06 20:53:49 -0700

    Updated documentation for MAX12 and MAX60 violations (v2024.0.0.2)

--- main_gui.py
+++ main_gui.py
@@ -145,7 +145,7 @@ class CustomTitleBar(QWidget):
 
 class MainApp(QMainWindow):
     # Class-level version info (single source of truth)
-    VERSION = "2024.0.0.1"  # Year.Major.Minor.Patch
+    VERSION = "2024.0.0.2"  # Year.Major.Minor.Patch
     BUILD_TIME = "2024-01-10"  # Build timestamp
 
     @staticmethod
