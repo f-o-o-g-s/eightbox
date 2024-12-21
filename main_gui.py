aec8350b43cd2dcc8ac46b09117dc846eb69cd43
f-o-o-g-s <17360806+f-o-o-g-s@users.noreply.github.com>
2024-12-20 21:07:53 -0700

    Feature update: Database sync and UI improvements (v2024.0.7.0)

--- main_gui.py
+++ main_gui.py
@@ -241,8 +241,8 @@ class MainApp(QMainWindow):
         otdl_maximization_pane (OTDLMaximizationPane): OTDL assignment interface
     """
 
-    VERSION = "2024.0.6.0"  # Updated by release.py
-    BUILD_TIME = "2024-12-16 12:21"  # Updated by release.py
+    VERSION = "2024.0.7.0"  # Updated by release.py
+    BUILD_TIME = "2024-12-20 21:07"  # Updated by release.py
     SETTINGS_FILE = "app_settings.json"  # File to store application settings
 
     def __init__(self):
