d3db76873cc41fb84f2c8054978188fc3b41408e
f-o-o-g-s <17360806+f-o-o-g-s@users.noreply.github.com>
2024-12-22 21:35:19 -0700

    Remove unused violation_detection_original.py reference file (v2024.1.0.2)

--- main_gui.py
+++ main_gui.py
@@ -241,8 +241,8 @@ class MainApp(QMainWindow):
         otdl_maximization_pane (OTDLMaximizationPane): OTDL assignment interface
     """
 
-    VERSION = "2024.1.0.1"  # Updated by release.py
-    BUILD_TIME = "2024-12-22 21:16"  # Updated by release.py
+    VERSION = "2024.1.0.2"  # Updated by release.py
+    BUILD_TIME = "2024-12-22 21:35"  # Updated by release.py
     SETTINGS_FILE = "app_settings.json"  # File to store application settings
 
     def __init__(self):
